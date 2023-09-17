c6ba4e2be5b555deb8b4f9bda28a9d7c7ec22d10
Genadij1 <bgennadij265@icloud.com>
2023-09-17 19:03:06 +0300

    2

--- Project1Wash/Header.h
+++ Project1Wash/Header.h
@@ -1,1 +1,28 @@
-#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Stiralka
+{
+private:
+	bool isOn;
+	string mode;
+	int temperature;
+	int time;
+	int speed;
+public:
+	Stiralka() { isOn = false; mode = ""; temperature = 0; time = 0; speed = 0; };
+	void turnOn() { cout << "\033[32mON\033[0m"; };
+	void turnOff() { cout << "OFF"; };
+	void setMode(string newMode) { this->mode = newMode; };
+	void setTemperature(int newTemperature) { this->temperature = newTemperature; };
+	void setTime(int newTime) { this->time = newTime; };
+	void setSpeed(int newSpeed) { this->speed = newSpeed; };
+	string GetChoice()
+	{
+		return "Mode: " + mode + "\nTemperature: " + to_string(temperature) + "\nTime: " + to_string(time) + "\nSpeed: " + to_string(speed);
+	}
+	~Stiralka() {};
+};
+
